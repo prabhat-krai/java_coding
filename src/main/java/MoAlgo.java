4a4b12f666dca83fe76b4ec65049a6075a54f902
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-10-26 23:50:20 +0530

    Thinking

--- src/main/java/MoAlgo.java
+++ src/main/java/MoAlgo.java
@@ -1,5 +1,8 @@
 package com.prabhat.exercise.top;
-
+//Square root decomposition is a method or a data structure,
+// which allows you to perform some typical operations
+// (summation of subarray, range minimum/maximum query, etc.)
+// in time complexity root of n, what is significantly better than O(n^2) when using trivial algorithm.
 public class MoAlgo {
     public static void main(String[] args) {
 
